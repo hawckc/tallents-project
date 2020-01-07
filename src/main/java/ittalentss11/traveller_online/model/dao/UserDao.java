32e11248e76f320f57f439a10128fc79ef71721e
hawckc <yahikoooooo@gmail.com>
2020-01-07 18:07:32 +0200

    no message

--- src/main/java/ittalentss11/traveller_online/model/dao/UserDao.java
+++ src/main/java/ittalentss11/traveller_online/model/dao/UserDao.java
@@ -1,5 +1,6 @@
 package ittalentss11.traveller_online.model.dao;
 
+import ittalentss11.traveller_online.model.dto.UserNoSensitiveDTO;
 import ittalentss11.traveller_online.model.dto.UserRegDTO;
 import ittalentss11.traveller_online.model.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import java.util.List;
 @Component
 public class UserDao {
     public static final String INSERT_USER = "INSERT INTO final_project.users (`first_name`, `last_name`, `username`, `password`, `email`) VALUES (?, ?, ?, ?, ?);";
+    public static final String USER_BY_USERNAME = "SELECT * FROM final_project.users WHERE username = ?;";
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -28,7 +30,7 @@ public class UserDao {
         return result == 0;
     }
     //USER REGISTRATION
-    public void register (User user){
+    public UserNoSensitiveDTO register (User user){
         //hash is made by taking password and salting
         jdbcTemplate.update(INSERT_USER,
                                 user.getFirstName(),
@@ -36,6 +38,7 @@ public class UserDao {
                                 user.getUsername(),
                                 UserRegDTO.hashSecurity(user.getPassword(), user.getUsername()),
                                 user.getEmail());
+        return new UserNoSensitiveDTO(user);
     }
     public boolean foundUsernameForLogin(String username){
         int result = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM final_project.users WHERE username = '"+username+"';", Integer.class);
@@ -47,16 +50,16 @@ public class UserDao {
     }
     public User getUserByUsername(String username) throws SQLException {
         Connection connection = jdbcTemplate.getDataSource().getConnection();
-        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM final_project.users WHERE username = ?;", Statement.RETURN_GENERATED_KEYS)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(USER_BY_USERNAME, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, "username");
             ResultSet set = preparedStatement.executeQuery();
             if (set.next()){
                 User u = new User(set.getLong("id"),
-                                    set.getString("first_name"),
-                                    set.getString("last_name"),
-                                    set.getString("username"),
-                                    set.getString("email"),
-                                    set.getString("password"));
+                        set.getString("first_name"),
+                        set.getString("last_name"),
+                        set.getString("username"),
+                        set.getString("email"),
+                        set.getString("password"));
                 return u;
             }
             return null;
