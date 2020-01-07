bce9d45dd6f680e6d8b61829618ca09b86c2da4b
hawckc <yahikoooooo@gmail.com>
2020-01-07 18:09:10 +0200

    no message

--- src/main/java/ittalentss11/traveller_online/model/pojo/Post.java
+++ src/main/java/ittalentss11/traveller_online/model/pojo/Post.java
@@ -1,16 +1,24 @@
 package ittalentss11.traveller_online.model.pojo;
 
+import ittalentss11.traveller_online.model.dto.LocationDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@Component
 public class Post {
     private long id;
+    @Autowired
     private User user;
+    @Autowired
     private Location location;
+    @Autowired
     private Category category;
     private String videoUrl;
     private String description;
