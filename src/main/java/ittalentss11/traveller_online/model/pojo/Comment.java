9c7c8900304a609d1f722f6a90f5267973659e53
hawckc <yahikoooooo@gmail.com>
2020-01-07 18:08:10 +0200

    no message

--- src/main/java/ittalentss11/traveller_online/model/pojo/Comment.java
+++ src/main/java/ittalentss11/traveller_online/model/pojo/Comment.java
@@ -3,10 +3,11 @@ package ittalentss11.traveller_online.model.pojo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @Getter
 @Setter
+@Component
 public class Comment {
     private long id;
     private String text;
