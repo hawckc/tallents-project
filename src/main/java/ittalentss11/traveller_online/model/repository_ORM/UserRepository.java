f479996fe0f760768a28d94cf84c89781d4a26cd
hawckc <yahikoooooo@gmail.com>
2020-01-08 19:42:05 +0200

    maven

--- src/main/java/ittalentss11/traveller_online/model/repository_ORM/UserRepository.java
+++ src/main/java/ittalentss11/traveller_online/model/repository_ORM/UserRepository.java
@@ -4,8 +4,9 @@ import ittalentss11.traveller_online.model.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 //TODO Finish up Repository folder and make corrections to old SQL queries
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
-
 }
---
+++ src/main/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Hawckc
+  Date: 8.1.2020 г.
+  Time: 19:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
