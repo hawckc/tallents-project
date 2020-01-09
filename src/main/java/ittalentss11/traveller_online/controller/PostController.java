9e857437a3420f5a871740062f2f6d70f54c6488
hawckc <yahikoooooo@gmail.com>
2020-01-10 01:01:13 +0200

    upload a picture to a post

--- src/main/java/ittalentss11/traveller_online/controller/PostController.java
+++ src/main/java/ittalentss11/traveller_online/controller/PostController.java
@@ -1,8 +1,9 @@
 package ittalentss11.traveller_online.controller;
-
 import ittalentss11.traveller_online.controller.controller_exceptions.AuthorizationException;
+import ittalentss11.traveller_online.controller.controller_exceptions.BadRequestException;
 import ittalentss11.traveller_online.controller.controller_exceptions.MissingCategoryException;
 import ittalentss11.traveller_online.controller.controller_exceptions.WrongCoordinatesException;
+
 import ittalentss11.traveller_online.model.dao.CategoryDAO;
 import ittalentss11.traveller_online.model.dao.PostDAO;
 import ittalentss11.traveller_online.model.dto.PostDTO;
@@ -11,17 +12,17 @@ import ittalentss11.traveller_online.model.pojo.Post;
 import ittalentss11.traveller_online.model.pojo.User;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
-
 @RestController
 public class PostController {
     @Autowired
     private PostDAO postDAO;
     @Autowired
     private CategoryDAO categoryDAO;
-
     //Posting a post:
     @SneakyThrows
     @PostMapping("/posts")
@@ -62,5 +63,25 @@ public class PostController {
         //CHANGED THE RETURN TYPE TO STRING BECAUSE WE'RE RETURNING THE CRYPTED PASSWORD when returning
         //THE USER JSON
         return "Your post was successfully added!";
+    }
+    @SneakyThrows
+    @PutMapping("/posts/{id}/upload")
+    public String addPicture(@RequestPart(value = "picture") MultipartFile multipartFile, @PathVariable("id") long id,
+                             HttpSession session){
+        //first we check if the use is logged
+        User user = (User) session.getAttribute(UserController.USER_LOGGED);
+        if (user == null){
+            throw new AuthorizationException();
+        }
+        // we check if there is a post with id
+        Post post = postDAO.getPostById(id);
+        if (post == null){
+            throw new BadRequestException();
+        }
+        // create a folder with the same name d:/tallents/png
+        //name the file picture_url_id.png
+        //move to folder
+        //write in posts_pictures post_id and picture url
+        return multipartFile.getName();
     }
 }
