68b1d4de5f1c168035d997d14f59cda0715f60c4
hawckc <yahikoooooo@gmail.com>
2020-01-09 03:34:53 +0200

    Wrong Request

--- src/main/java/ittalentss11/traveller_online/controller/controller_exceptions/WrongRequest.java
+++ src/main/java/ittalentss11/traveller_online/controller/controller_exceptions/WrongRequest.java
@@ -1,4 +1,12 @@
 package ittalentss11.traveller_online.controller.controller_exceptions;
 
-public class WrongRequest {
+public class WrongRequest extends Exception {
+
+    public WrongRequest(String s){
+
+        super(s);
+    }
+    public WrongRequest(){
+        super("Wrong request");
+    }
 }
