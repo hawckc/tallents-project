11d775210ce7d9acc53eef0f44c6610721631a5a
hawckc <yahikoooooo@gmail.com>
2020-01-10 00:26:34 +0200

    added categories and locations exceptions

--- src/main/java/ittalentss11/traveller_online/controller/controller_exceptions/MissingCategoryException.java
+++ src/main/java/ittalentss11/traveller_online/controller/controller_exceptions/MissingCategoryException.java
@@ -1,4 +1,12 @@
 package ittalentss11.traveller_online.controller.controller_exceptions;
 
-public class MissingCategoryException {
+public class MissingCategoryException extends Exception {
+    public MissingCategoryException(String s){
+        super(s);
+
+    }
+    public MissingCategoryException(){
+        super("Invalid category");
+    }
+
 }
--- src/main/java/ittalentss11/traveller_online/controller/controller_exceptions/WrongCoordinatesException.java
+++ src/main/java/ittalentss11/traveller_online/controller/controller_exceptions/WrongCoordinatesException.java
@@ -1,4 +1,10 @@
 package ittalentss11.traveller_online.controller.controller_exceptions;
+public class WrongCoordinatesException extends Exception {
+    public WrongCoordinatesException(String s){
+        super(s);
+    }
 
-public class WrongCoordinatesException {
+    public WrongCoordinatesException(){
+        super("Wrong coordinates format. The format should be number number");
+    }
 }
