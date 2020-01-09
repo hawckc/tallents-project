b6921bf0209d6cd637cf13b9eca304e86da41035
hawckc <yahikoooooo@gmail.com>
2020-01-09 03:35:34 +0200

    Comment DTO

--- src/main/java/ittalentss11/traveller_online/model/dto/CommentDTO.java
+++ src/main/java/ittalentss11/traveller_online/model/dto/CommentDTO.java
@@ -1,4 +1,13 @@
 package ittalentss11.traveller_online.model.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDTO {
+    private String comment;
 }
