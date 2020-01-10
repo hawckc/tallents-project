7a73778856d658b7fd1ce2e2697ebb62a3b09b58
Georgi Bozukov <gs.bozukov@gmail.com>
2020-01-10 17:16:16 +0200

    Users can now like a post, changed the Post and User pojos a little in order for it to work

--- src/main/java/ittalentss11/traveller_online/model/pojo/User.java
+++ src/main/java/ittalentss11/traveller_online/model/pojo/User.java
@@ -1,12 +1,13 @@
 package ittalentss11.traveller_online.model.pojo;
 import ittalentss11.traveller_online.model.dto.UserRegDTO;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+//equals and hashcode is for many to many (likes)
+@EqualsAndHashCode
 @NoArgsConstructor
 @Getter
 @Setter
@@ -28,12 +29,24 @@ public class User {
     private String email;
     @Column
     private String password;
-    
+    //TODO: for likes
+    @ManyToMany(mappedBy = "usersThatLiked")
+    private List<Post> likedPosts = new ArrayList<>();
+    //
     public User(UserRegDTO userRegDTO){
         this.firstName = userRegDTO.getFirstName();
         this.lastName = userRegDTO.getLastName();
         this.username = userRegDTO.getUsername();
         this.email = userRegDTO.getEmail();
         this.password = userRegDTO.getPassword();
+    }
+    //TODO: for likes
+    public User (long id, String firstName, String lastName, String username, String email, String password){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
 }
