14da1d53e7a92e684d1fa7bfb0c99692d0e082e4
hawckc <yahikoooooo@gmail.com>
2020-01-10 20:19:11 +0200

    dao changes

--- src/main/java/ittalentss11/traveller_online/model/dao/PostPictureDao.java
+++ src/main/java/ittalentss11/traveller_online/model/dao/PostPictureDao.java
@@ -1,4 +1,37 @@
 package ittalentss11.traveller_online.model.dao;
+import ittalentss11.traveller_online.model.pojo.Post;
+import ittalentss11.traveller_online.model.pojo.PostPicture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import org.springframework.stereotype.Component;
+import java.sql.*;
+@Component
 public class PostPictureDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private static final String INSERT_PICTURE = "INSERT INTO final_project.post_pictures (posts_id, picture_url)" +
+            " VALUES (?, ?);";
+
+    public void addPostPicture(Post post, String name) throws SQLException {
+        Connection connection = jdbcTemplate.getDataSource().getConnection();
+        //String[] picturewithall = postPicture.getPictureUrl().split(".");
+        //String picture = picturewithall[0];
+        //picture += "_";
+        //picture += postPicture.getPost().getUser().getId();
+        PostPicture postPicture = new PostPicture();
+        postPicture.setPost(post);
+        try(PreparedStatement ps = connection.prepareStatement(INSERT_PICTURE, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, (int) post.getId());
+            ps.setString(2, name);
+            ps.executeUpdate();
+            ResultSet keys = ps.getGeneratedKeys();
+            keys.next();
+            postPicture.setId(keys.getLong(1));
+        }
+    }
+    public int getAllPictures(int id){
+        int result = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM final_project.post_pictures WHERE posts_id = ?;", new Object[]{id}, Integer.class);
+        return result;
+    }
 }
