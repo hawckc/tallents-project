402a9dc06d62fa637cb0752b06295b4ca62fcd27
hawckc <yahikoooooo@gmail.com>
2020-01-11 17:36:34 +0200

    post upload of videos

--- src/main/java/ittalentss11/traveller_online/controller/PostController.java
+++ src/main/java/ittalentss11/traveller_online/controller/PostController.java
@@ -86,27 +86,23 @@ public class PostController {
         if (post.getUser().getUsername().equals(user.getUsername()) == false){
             throw new BadRequestException();
         }
-        String namesWhole = multipartFile.getOriginalFilename();
-        String path = "C://posts//png//";
-        String[] all = namesWhole.split("\\.", 2);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yy-MM-dd-hh-mm-ss");
-        LocalDateTime localDateTime = LocalDateTime.now();
-        String parse = localDateTime.format(dateTimeFormatter);
-        String nameWithout = all[0];
-        String formatForPicture = all[1];
-        String nameWithId = all[0] + "_" + parse + "_" + user.getId() + "." + all[1];
-        System.out.println(namesWhole);
-        if (formatForPicture.equals("png") == false){
+        if (multipartFile == null){
             throw new BadRequestException();
         }
-        File picture = new File(path + nameWithId);
+        String path = "C://posts//png//";
+        //System.out.println(namesWhole);
+        String pictureName = PostController.getNameForUpload(multipartFile.getOriginalFilename(), path, user);
+        if (pictureName.split("\\.")[1].equals("png") == false){
+            throw new BadRequestException();
+        }
+        File picture = new File(pictureName);
         FileOutputStream fos = new FileOutputStream(picture);
         fos.write(multipartFile.getBytes());
         fos.close();
         PostPicture postPicture = new PostPicture();
-        postPictureDao.addPostPicture(post, nameWithId);
+        postPictureDao.addPostPicture(post, pictureName);
         postPicture.setPost(post);
-        postPicture.setPictureUrl(nameWithId);
+        postPicture.setPictureUrl(pictureName);
         //check if there are no more than 3 pictures
         //check if the file is a picture
         // create a folder with the same name d:/tallents/png
@@ -114,6 +110,50 @@ public class PostController {
         //move to folder
         //write in posts_pictures post_id and picture url
         return picture.getName();
+    }
+    @SneakyThrows
+    @PutMapping("/posts/{id}/videos")
+    public String addVideos(@RequestPart(value = "videos") MultipartFile multipartFile, @PathVariable("id") long id,
+                             HttpSession session){
+        //first we check if the use is logged
+        User user = (User) session.getAttribute(UserController.USER_LOGGED);
+        if (user == null){
+            throw new AuthorizationException();
+        }
+        // we check if there is a post with id
+        Post post = postDAO.getPostById(id);
+        if (post == null){
+            throw new BadRequestException();
+        }
+        if (post.getUser().getUsername().equals(user.getUsername()) == false){
+            throw new BadRequestException();
+        }
+        if (multipartFile == null){
+            throw new BadRequestException();
+        }
+        String path = "C://posts//videos//";
+        String videosName = PostController.getNameForUpload(multipartFile.getOriginalFilename(), path, user);
+        //System.out.println(namesWhole);
+        if (videosName.split("\\.")[1].equals("avi") == false){
+            throw new BadRequestException();
+        }
+        File videos = new File(videosName);
+        FileOutputStream fos = new FileOutputStream(videos);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        postDAO.addVideos(post, videosName);
+        return videos.getName();
+    }
+    public static String getNameForUpload(String name, String path, User user){
+        String[] all = name.split("\\.", 2);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yy-MM-dd-hh-mm-ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String parse = localDateTime.format(dateTimeFormatter);
+        String nameWithout = all[0];
+        String formatForPicture = all[1];
+        String nameWithId = all[0] + "_" + parse + "_" + user.getId() + "." + all[1];
+        System.out.println(name);
+        return path + nameWithId;
     }
 
     //TODO: make sure to clean up post repositories in likes and post controller, make them in DAOs
