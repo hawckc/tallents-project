f63fe33c8d14db31ce360298bce3fd147b1fe99a
hawckc <yahikoooooo@gmail.com>
2020-01-12 16:39:07 +0200

    search with username

--- src/main/java/ittalentss11/traveller_online/model/dao/PostDAO.java
+++ src/main/java/ittalentss11/traveller_online/model/dao/PostDAO.java
@@ -1,5 +1,7 @@
 package ittalentss11.traveller_online.model.dao;
 import ittalentss11.traveller_online.controller.controller_exceptions.BadRequestException;
+import ittalentss11.traveller_online.model.dto.PostDTO;
+import ittalentss11.traveller_online.model.dto.ViewPostDTO;
 import ittalentss11.traveller_online.model.pojo.Post;
 import ittalentss11.traveller_online.model.repository_ORM.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -21,8 +25,8 @@ public class PostDAO {
             "INSERT INTO final_project.posts " +
             "(user_id, video_url, description , other_info, category_id, coordinates, map_url, location_name) " +
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
-
     public static final String UPDATE_POST_FOR_VIDEOS = "UPDATE final_project.posts SET video_url = ? WHERE id = ?;";
+    public static final String GET_POSTS_BY_USERNAME = "SELECT p.* FROM final_project.posts JOIN users AS un ON p.user_id = un.id WHERE un.username LIKE CONCAT('%', ? ,'%');";
     //User post a post
     public void addPost(Post post) throws SQLException {
         Connection connection = jdbcTemplate.getDataSource().getConnection();
@@ -62,5 +66,26 @@ public class PostDAO {
 
     public void save(Post post) {
         postRepository.save(post);
+    }
+    public ArrayList<ViewPostDTO> getPostsByUsername(String username) throws SQLException {
+        Connection connection = jdbcTemplate.getDataSource().getConnection();
+        try(PreparedStatement ps = connection.prepareStatement(GET_POSTS_BY_USERNAME, Statement.RETURN_GENERATED_KEYS)){
+            ps.setString(1, username);
+            ResultSet set = ps.executeQuery();
+            ArrayList<ViewPostDTO> arr = new ArrayList<>();
+            while(set.next()){
+                ViewPostDTO postDTO = new ViewPostDTO();
+                postDTO.setMapUrl(set.getString("map_url"));
+                postDTO.setCoordinates(set.getString("coordinates"));
+                postDTO.setLocationName(set.getString("location_name"));
+                postDTO.setUserId(set.getInt("user_id"));
+                postDTO.setCategoryId(set.getInt("category_id"));
+                postDTO.setVideoUrl(set.getString("video_url"));
+                postDTO.setOtherInfo(set.getString("other_info"));
+                arr.add(postDTO);
+            }
+            return arr;
+
+        }
     }
 }
